c34079f6341b5ce1823cdf860e992e0bf9633f7e
Louis Born <louis.born@check24.de>
2022-11-23 13:26:41 +0100

    removed comment

--- do_not_modify/sql_handler.py
+++ do_not_modify/sql_handler.py
@@ -52,7 +52,7 @@ class SQLConnectionHandler:
         try:
             self.connection = pymysql.connect(
                 host='127.0.0.1',
-                user=self.credentials["database_username"],  # Modify here for error demo
+                user=self.credentials["database_username"],
                 passwd=self.credentials["database_password"],
                 db=self.credentials["database_name"],
                 port=tunnel.local_bind_port
