e20fa61b9cfb060e4e1c3f39526cd8143346ae50
Rojal <Rojal.mohammad1@gmail.com>
2022-12-07 15:52:01 +0100

    Rojal added Hello World

--- group_assessment/assessment.py
+++ group_assessment/assessment.py
@@ -1,5 +1,6 @@
 from do_not_modify.sql_handler import SQLHandlerFacade
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 class GroupAssessment:
     def __init__(self, app):
@@ -8,14 +9,21 @@ class GroupAssessment:
 
     def operation(self) -> dict:
         handler = SQLHandlerFacade(app=self.app, query="SELECT * FROM mdl_quiz_grades")
-        operation_result, pd_dataframe = handler.operation()
+        handler2 = SQLHandlerFacade(app= self.app, query="SELECT * FROM mdl_assign_grades")
+        quiz_grades, pd_dataframe = handler.operation()
+        assign_grades, pd_dataframe2 = handler2.operation()
         # Work with pandas.
         print(pd_dataframe.head())
-        print(pd_dataframe["id"])
-        print(pd_dataframe.loc[:5, ["id", "grade"]])
-        print(pd_dataframe.iloc[0])
-        print(pd_dataframe[pd_dataframe["grade"] == '8.50000'])
-        print(pd_dataframe[pd_dataframe["timemodified"].isin([1577442881, 1577442882])])
+        print(pd_dataframe2.head())
+        #id_grades=pd_dataframe["id"]
+        #print(id_grades)
+        print(pd_dataframe)
+        #print(pd_dataframe.loc[:5, ["id", "grade"]])
+        #print(pd_dataframe.iloc[0])
+        #print(pd_dataframe[pd_dataframe["grade"] == '8.50000'])
+        #print(pd_dataframe[pd_dataframe["timemodified"].isin([1577442881, 1577442882])])
+
         # Work with json data.
-        data = operation_result.get("result")
-        return operation_result
+        data = quiz_grades.get("result")
+        data2 = assign_grades.get("result")
+        return quiz_grades, assign_grades
