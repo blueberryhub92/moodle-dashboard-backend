9a355512f5318e2d912b5113bc8c2f157619cba7
marlahendrich <marlahendrich@gmx.de>
2022-12-10 14:49:08 +0100

    filter and links

--- group_overall_progress/overall_progress.py
+++ group_overall_progress/overall_progress.py
@@ -1,20 +1,58 @@
 from do_not_modify.sql_handler import SQLHandlerFacade
+import time
 
 
 class GroupOverallProgress:
     def __init__(self, app):
         self.app = app
 
+    moduleData = []
+    logData = []
+    moodleURL = 'http://83.212.126.199/moodle/mod/'
+
     def operation(self) -> dict:
-        handler = SQLHandlerFacade(app=self.app, query="SELECT * FROM mdl_quiz_grades")
+        handler = SQLHandlerFacade(app=self.app, query="SELECT * FROM mdl_course_modules WHERE visible=1 ORDER BY added ASC")
         operation_result, pd_dataframe = handler.operation()
-        # Work with pandas.
-        print(pd_dataframe.head())
-        print(pd_dataframe["id"])
-        print(pd_dataframe.loc[:5, ["id", "grade"]])
-        print(pd_dataframe.iloc[0])
-        print(pd_dataframe[pd_dataframe["grade"] == '8.50000'])
-        print(pd_dataframe[pd_dataframe["timemodified"].isin([1577442881, 1577442882])])
-        # Work with json data.
+
         data = operation_result.get("result")
+        self.moduleData = data
+        print(time.mktime(time.strptime('20/10/2020', "%d/%m/%Y")))
+        self.filterTime(1450847576, 1578329680)
+        # self.printModules()
+        self.operation2()
         return operation_result
+
+    def operation2(self) -> dict:
+        handler = SQLHandlerFacade(app=self.app,
+                                   query="SELECT * FROM logs_LA_20__21_20221202-1706")
+        operation_result, pd_dataframe = handler.operation()
+
+        logs = operation_result.get("result")
+        self.logData = logs
+        print(self.logData)
+
+        self.redirect('URL', '4')
+        self.redirect('File', '6')
+
+        return operation_result
+
+    def redirect(self, component, id):
+        if component == 'URL':
+            link = self.moodleURL+'url/view.php?id='+id
+        elif component == 'File':
+            link = self.moodleURL + 'resource/view.php?id=' + id
+        print(link)
+    def filterTime(self, start, stop):
+        filteredData = []
+        for module in self.moduleData:
+            if (module['added'] >= start) and (module['added'] <= stop):
+                filteredData.append(module)
+        self.moduleData = filteredData
+        return filteredData
+
+    # http://83.212.126.199/moodle/mod/resource/view.php?id=6
+    # http://83.212.126.199/moodle/mod/url/view.php?id=4
+    def printModules(self):
+        for module in self.moduleData:
+            print('id: ', module['id'])
+            # print('timestamp: ', module['added'])
